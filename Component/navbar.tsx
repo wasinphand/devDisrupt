a26c8771423793167ed060ed40d4f408b06385eb
Theme <5batman400@gmail.com>
2020-11-01 01:55:32 +0700

    style main page

---
+++ Component/navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Box} from "@chakra-ui/core"
+
+const Navbar = () => {
+  return (
+    <>
+      <Box bg="white" w="100%" p={4} shadow="md">
+        LOGO
+      </Box>
+    </>
+  )
+}
+
+
+export default Navbar
