4a856c809c9d396567d24d253f554c358acf8aa2
NatthakornS <65764310+NatthakornS@users.noreply.github.com>
2020-11-01 00:17:08 +0700

    ..

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,22 @@
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
-import {Header} from '../Component/Header';
-import { ThemeProvider, CSSReset } from '@chakra-ui/core'
-import Boxes from '../Component/Boxes';
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import { Header } from "../Component/Header";
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+import Boxes from "../Component/Boxes";
 
 export default function Home() {
-
   return (
     <ThemeProvider>
-            <CSSReset/>
+      <CSSReset />
       <div className={styles.container}>
         <Header />
-        <div className={styles.banner} > 
-        100% of your donation via Socialgiver goes to social projects!
+        <div className={styles.banner}>
+          100% of your donation via Socialgiver goes to social projects!
         </div>
-        <Boxes/>
-        
+        <Boxes />
       </div>
-       <CSSReset />
-    <div className={styles.container}>
-      
-    </div>
+      <CSSReset />
+      <div className={styles.container}></div>
     </ThemeProvider>
-  )
+  );
 }
