ff3dfa17f5a9638746bd87b30066d6238db13654
wasin <wasinph@outlook.com>
2020-10-31 18:04:30 +0700

    add Chakra UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,15 @@
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
-import {fetchData} from '../hookApi/testApi';
-import {useEffect,useState} from 'react';
+
 
 export default function Home() {
-  const [data,setData] = useState([]);
 
-  useEffect(() => {
-    fetchData().then(function (res){
-      setData(res);
-    })
 
-  },[data])
 
 
   return (
     <div className={styles.container}>
       Hello NextJS
-  
       <ul>
         <li>
           <Link href="/">
