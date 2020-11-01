37473b8f8554c08f12e2651b4c8717442436995a
wasin <wasinph@outlook.com>
2020-11-01 10:23:13 +0700

    add summary

--- pages/summary.tsx
+++ pages/summary.tsx
@@ -1,6 +1,8 @@
 import styles from '../styles/Home.module.css'
 import { Icon } from "@chakra-ui/core";
 import { ThemeProvider, CSSReset } from '@chakra-ui/core'
+import { Box, Heading, Image, Text, Stack, Progress } from "@chakra-ui/core";
+
 
 const download = e => {
     console.log(e.target.href);
@@ -25,10 +27,19 @@ const download = e => {
 const Summary = () => (
     <ThemeProvider>
                <CSSReset/>
-    <div>
-        <b style={{fontSize:"30px"}}>
-            สรุปผลการดำเนินการ โครงการ COVID-19
-        </b>
+    <div >
+        <Box
+            bg="white"
+            p={4}
+            shadow="md"
+            display="flex"
+            flexDir="column"
+            rounded="lg"
+        >
+            <b style={{fontSize:"30px",color:'teal'}}>
+                สรุปผลการดำเนินการ โครงการ COVID-19
+            </b>
+        </Box>
        <table className={styles.table}>
            <tr  className={styles.table}>
                <th  className={styles.table}><b>วันที่</b></th>
@@ -75,7 +86,55 @@ const Summary = () => (
                <th onClick={(e)=>download(e)} className={styles.table}>   <Icon name="download" size="24px" />  </th>
            </tr>
        </table>
-       
+       <Box
+            bg="white"
+            p={4}
+            shadow="md"
+            display="flex"
+            flexDir="column"
+            rounded="lg"
+            style = {{marginBottom:"35px"}}
+        >
+           <div>  <b style={{fontSize:"20px",color:"teal"}}>ผลการดำเนินงาน</b></div>
+            <div style={{color:"blue"}}>โครงการ Thailand Recovery Fund ก่อตั้งโดยทีม Socialgiver มีเป้าหมายหลักในการระดมทุน จากการดำเนินงานต่างๆที่เราได้ทำไป เราได้ใช้งบประมาณที่ได้รับมา ไปซื้ออุปกรณ์ต่างๆตามที่แจ้งไว้ด้านบน
+            จากที่เราได้ ดำเนินงานไป ได้รับผลการตอบรับจากหน่วยงานต่างๆเป็นอย่างดี และ ได้ช่วยสร้าง impact ให้กับสังคมเป็นอย่างมาก มีเป้าหมายหลักในการระดมทุน จากการดำเนินงานต่างๆที่เราได้ทำไป เราได้ใช้งบประมาณที่ได้รับมา ไปซื้ออุปกรณ์ต่างๆตามที่แจ้งไว้ด้านบน
+            เราได้ใช้งบประมาณที่ได้รับมา ไปซื้ออุปกรณ์ต่างๆตามที่แจ้งไว้ด้านบน</div>
+       </Box>
+       <Box
+            bg="white"
+            p={4}
+            shadow="md"
+            display="flex"
+            flexDir="column"
+            rounded="lg"
+            style = {{marginBottom:"35px"}}
+        >
+        <div>  <b style={{fontSize:"20px",color:"teal"}}>ทีมในการจัดงาน</b></div>
+        <ol style={{color:"blue",paddingLeft:"20px"}}>
+            <li>John S.</li>
+            <li>James A.</li>
+            <li>Random SS.</li>
+        </ol>
+       </Box>
+       <Box
+            bg="white"
+            p={4}
+            shadow="md"
+            display="flex"
+            flexDir="column"
+            rounded="lg"
+            style = {{marginBottom:"35px"}}
+        >
+       <div> <b style={{fontSize:"20px",color:"teal"}}>ผู้สนับสนุนสูงสุด 5 อันดับแรก</b></div>
+       <ol style={{color:"blue",paddingLeft:"20px"}}>
+           <li>John S. จำนวน ฿1,000,000 </li>
+           <li>James A. จำนวน ฿800,000</li>
+           <li>Random SS. จำนวน ฿500,000</li>
+           <li>James A. จำนวน ฿400,000</li>
+           <li>Random SS. จำนวน ฿300,000</li>
+       </ol>
+       </Box>
+      
     </div>
     </ThemeProvider>
 )
