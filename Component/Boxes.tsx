7cd6db9dc758ab947040aea86dfca226b6fc71f5
wasin <wasinph@outlook.com>
2020-11-01 15:11:22 +0700

    ...

--- Component/Boxes.tsx
+++ Component/Boxes.tsx
@@ -1,6 +1,6 @@
 import { Box, Image, SimpleGrid } from "@chakra-ui/core";
 import { datas } from "../hookApi/mockData";
-import Card from "./Card";
+import Card from "./card";
 
 export default function Boxes() {
   return (
